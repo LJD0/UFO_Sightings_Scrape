c72297645c8f3b24df004a42d00de6032292d56e
OAO-MadHatter <larsenj2064@gmail.com>
2022-11-23 13:38:44 -0800

    12.5 Final Commit
    added filter

--- static/js/app.js
+++ static/js/app.js
@@ -16,4 +16,15 @@ function buildTable (data){
             cell.text(val);
         });
     });
-}
+};
+
+function handleClick () {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    buildTable(filteredData)
+};
+d3.selectAll("#filter-btn").on("click", handleClick);
+buildTable(tableData)
